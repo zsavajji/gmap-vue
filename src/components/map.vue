73b135e82c29ccd949c388b78463ae6a78187a2b
daniel_sim <daniel_sim@data.gov.sg>
2016-12-24 17:15:57 +0800

    add a separate slot for visible and hidden child components

--- src/components/map.vue
+++ src/components/map.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="vue-map-container">
     <div ref="vue-map" class="vue-map"></div>
-    <slot></slot>
+    <div class="vue-map-hidden">
+      <slot></slot>
+    </div>
+    <slot name="visible"></slot>
   </div>
 </template>
 
@@ -16,5 +19,8 @@
 .vue-map-container .vue-map {
   left: 0; right: 0; top: 0; bottom: 0;
   position: absolute;
+}
+.vue-map-hidden {
+  display: none;
 }
 </style>
