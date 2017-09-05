c2e1a4b98346ed04fcfda3e7f5b235c0a8a1ea99
daniel_sim <daniel_sim@data.gov.sg>
2017-09-05 22:06:51 +0800

    a new examples page showing source code

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,8 +1,41 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs');
 
-module.exports = {
-  entry: './src/main.js',
+// Write out the list of examples to the examples index
+const examplesDir = path.resolve(__dirname, 'components')
+const examplesIndex = fs.readdirSync(
+  examplesDir
+)
+  .filter(f => f.endsWith('.vue'))
+  .map(f => {
+    const baseFileName = path.basename(f, '.vue')
+
+    return `
+import E${baseFileName}_Source from ${JSON.stringify(`!!raw-loader!${examplesDir}/${f}`)}
+import E${baseFileName}_Module from ${JSON.stringify(`${examplesDir}/${f}`)}
+
+examples.push({
+  name: ${JSON.stringify(baseFileName)},
+  module: E${baseFileName}_Module,
+  source: E${baseFileName}_Source,
+  description: E${baseFileName}_Module.description,
+})
+    `
+  })
+
+fs.writeFileSync(
+  path.resolve(__dirname, 'examples-index.js'),
+  `
+const examples = []
+
+${examplesIndex.join('\n')}
+
+export default examples
+  `
+)
+
+const base = {
   output: {
     path: path.resolve('./dist'),
     publicPath: 'dist/',
@@ -13,6 +46,11 @@ module.exports = {
       {
         test: /\.vue$/,
         loader: 'vue-loader',
+        options: {
+          loaders: {
+            js: 'babel-loader'
+          }
+        }
       },
       {
         test: /\.js$/,
@@ -28,8 +66,9 @@ module.exports = {
   },
 };
 
+
 if (process.env.NODE_ENV === 'production') {
-  module.exports.plugins = [
+  base.plugins = [
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
@@ -42,5 +81,10 @@ if (process.env.NODE_ENV === 'production') {
     }),
   ];
 } else {
-  module.exports.devtool = 'source-map';
+  base.devtool = 'source-map';
 }
+
+module.exports = [
+  Object.assign({}, base, {entry: './src/main.js', output: Object.assign({}, base.output, {filename: 'build.js'})}),
+  Object.assign({}, base, {entry: './src/ExamplesMain.js', output: Object.assign({}, base.output, {filename: 'build-examples.js'})}),
+]
