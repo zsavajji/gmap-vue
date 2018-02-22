d0b2cfd90cb2d251c25ad3714d5822e5c192650d
daniel sim <daniel@LAPTOP-VIQA6AQL.localdomain>
2018-02-22 13:32:20 +0800

    only launch one puppeteer per suite

--- test/test-common.js
+++ test/test-common.js
@@ -2,16 +2,19 @@ import Puppeteer from 'puppeteer'
 import CompileStandalone from './compile-standalone'
 import path from 'path'
 
+const pagePromise = CompileStandalone.then(() => {
+  let options = {}
+  
+  if (process.env['THIS_IS_ON_TRAVIS_AND_SANDBOX_IS_NOT_ALLOWED'] === 'true') {
+    options.args = ['--no-sandbox', '--disable-setuid-sandbox']
+  }
+
+  return Puppeteer.launch(options).then(browser => browser.newPage())
+})
+
 export function getPage (p) {
   return async () => {
-    let options = {}
-
-    if (process.env['THIS_IS_ON_TRAVIS_AND_SANDBOX_IS_NOT_ALLOWED'] === 'true') {
-      options.args = ['--no-sandbox', '--disable-setuid-sandbox']
-    }
-
-    await CompileStandalone
-    p(await Puppeteer.launch(options).then(browser => browser.newPage()))
+    p(await pagePromise)
   }
 }
 
