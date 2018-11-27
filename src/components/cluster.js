2cdf062bb76e0a4d16091bd707c9760294b034e6
Daniel Sim <daniel.ssq89@gmail.com>
2018-11-27 16:46:53 +0800

    remove trailing spaces

--- src/components/cluster.js
+++ src/components/cluster.js
@@ -3,7 +3,7 @@
   * @prop $clusterObject -- Exposes the marker clusterer to
         descendent Marker classes. Override this if you area
         extending the class
-        
+
   List of properties from
   https://github.com/googlemaps/v3-utility-library/blob/master/markerclustererplus/src/markerclusterer.js
 **/
