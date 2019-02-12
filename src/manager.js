11e3aa087c05c5e152bdd19412b3eaf7c6b64d44
Thomas Haukland <thomas.haukland@gmail.com>
2019-02-12 15:17:52 +0100

    Change China maps api from http to https (#581)
    * Test https china google maps api
    
    * test https for china

--- src/manager.js
+++ src/manager.js
@@ -55,7 +55,7 @@ export const loadGmapApi = (options, loadCn) => {
     let baseUrl = 'https://maps.googleapis.com/'
 
     if (typeof loadCn === 'boolean' && loadCn === true) {
-      baseUrl = 'http://maps.google.cn/'
+      baseUrl = 'https://maps.google.cn/'
     }
 
     let url = baseUrl + 'maps/api/js?' +
