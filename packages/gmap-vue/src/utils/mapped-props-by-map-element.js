74d9393d8a2bd18b7cb2f8c8c1f88809d74702cb
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2020-10-18 17:43:19 -0300

    refactor(gmap-vue): create a unique mapped-props file

---
+++ packages/gmap-vue/src/utils/mapped-props-by-map-element.js
@@ -0,0 +1,39 @@
+/**
+ * This are GoogleMapsOptions that we want to have like
+ * props in our Vue component.
+ * This properties are in the way that GoogleMaps accept it
+ * and with extraneous properties for the VueJs API.
+ * In a previous version of this plugin, to avoid repetition,
+ * we created a .js file component with a `mappedProps` key on it
+ * and used a variety of helper functions to clean it and bind it
+ * to Vue props and watch them, etc.
+ * To day our primary main goal is get a more intuitive and descriptive
+ * API and a better documentation of it, following this goals we move
+ * this extraneous properties to an independent file in order to consume
+ * it when is needed.
+ * Please you need to remind that you need to create properties in the
+ * correspondent Vue component with the same names and the same values
+ * for those properties that are not extraneous to Vue.
+ */
+
+export const autocompleteProps = {
+  bounds: {
+    type: Object,
+  },
+  componentRestrictions: {
+    type: Object,
+    // Do not bind -- must check for undefined
+    // in the property
+    noBind: true,
+  },
+  types: {
+    type: Array,
+    default() {
+      return [];
+    },
+  },
+};
+
+export default {
+  autocompleteProps,
+};
