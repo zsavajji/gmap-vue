7b66499c40ecf3ec960b39cb6c1ad4b3b4d458dc
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2020-11-05 02:16:10 -0300

    refactor(gmap-vue): refactor of circle component ready

--- packages/gmap-vue/src/components/circle.vue
+++ packages/gmap-vue/src/components/circle.vue
@@ -3,24 +3,63 @@ import mapElementMixin from '../mixins/map-element';
 import { circleMappedProps } from '../utils/mapped-props-by-map-element';
 import { bindEvents, getPropsValues, bindProps } from '../utils/helpers';
 
+/**
+ * Circle component
+ * @displayName GmapCircle
+ * @see [source code](/guide/circle.html)
+ */
 export default {
   mixins: [mapElementMixin],
   props: {
+    /**
+     * The center of the circle
+     * @value { lat: 41.878, lng: -87.629 }
+     * @see [Circle simple](https://developers.google.com/maps/documentation/javascript/examples/circle-simple)
+     */
     center: {
       type: Object,
       required: true,
     },
+    /**
+     * The radious of the circle
+     * @value 10
+     * @see [Circle simple](https://developers.google.com/maps/documentation/javascript/examples/circle-simple)
+     */
     radius: {
       type: Number,
     },
+    /**
+     * Indicates if the circle is draggable
+     * @value true, false
+     * @see [Circle simple](https://developers.google.com/maps/documentation/javascript/examples/circle-simple)
+     */
     draggable: {
       type: Boolean,
       default: false,
     },
+    /**
+     * Indicates if the circle is editable
+     * @value true, false
+     * @see [Circle simple](https://developers.google.com/maps/documentation/javascript/examples/circle-simple)
+     */
     editable: {
       type: Boolean,
       default: false,
     },
+    /**
+     * The Google Maps circle options
+     * @value {
+        strokeColor: "#FF0000",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#FF0000",
+        fillOpacity: 0.35,
+        map,
+        center: citymap[city].center,
+        radius: Math.sqrt(citymap[city].population) * 100,
+      }
+     * @see [Circle simple](https://developers.google.com/maps/documentation/javascript/examples/circle-simple)
+     */
     options: {
       type: Object,
     },
