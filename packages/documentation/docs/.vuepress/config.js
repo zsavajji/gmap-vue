1dfed1377becab61e547a0586f6437ee43cad1e8
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2021-11-27 17:04:42 -0300

    fix(docs): fix documentation issues with webpack config and build

--- packages/documentation/docs/.vuepress/config.js
+++ packages/documentation/docs/.vuepress/config.js
@@ -1,57 +1,64 @@
-const { sidebarTree } = require('../code/config');
-const apiDocs = sidebarTree('API docs');
+const { sidebarTree } = require("../code/config");
+const apiDocs = sidebarTree("API docs");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
 module.exports = {
-  base: '/gmap-vue/',
-  title: 'Gmap-vue plugin',
-  description: 'Documentation for gmap-vue plugin',
+  base: "/gmap-vue/",
+  title: "Gmap-vue plugin",
+  description: "Documentation for gmap-vue plugin",
   themeConfig: {
     sidebarDepth: 4,
     nav: [
-      { text: 'Plugin', link: '/' },
-      { text: 'Guide', link: '/guide/' },
-      { text: 'API', link: '/code/' },
-      { text: 'GitHub', link: 'https://github.com/diegoazh/gmap-vue' },
+      { text: "Plugin", link: "/" },
+      { text: "Guide", link: "/guide/" },
+      { text: "API", link: "/code/" },
+      { text: "GitHub", link: "https://github.com/diegoazh/gmap-vue" },
     ],
     sidebar: {
-      '/code/': apiDocs['/code/'],
-      '/guide/': [
+      "/code/": apiDocs["/code/"],
+      "/guide/": [
         {
-          title: 'Guide',
+          title: "Guide",
           collapsable: false,
           children: [
             {
-              title: 'Concepts',
+              title: "Concepts",
               collapsable: false,
-              path: '/guide/'
+              path: "/guide/",
             },
             {
-              title: 'Components',
+              title: "Components",
               collapsable: false,
-              path: '/guide/map',
+              path: "/guide/map",
               children: [
-                ['map', 'GmapMap'],
-                ['autocomplete', 'GmapAutocomplete'],
-                ['circle', 'GmapCircle'],
-                ['cluster', 'GmapCluster'],
-                ['drawing-manager', 'GmapDrawingManager'],
-                ['heatmap-layer', 'GmapHeatmapLayer'],
-                ['info-window', 'GmapInfoWindow'],
-                ['kml-layer', 'GmapKmlLayer'],
-                ['marker', 'GmapMarker'],
-                ['place-input', 'GmapPlaceInput'],
-                ['polygon', 'GmapPolygon'],
-                ['polyline', 'GmapPolyline'],
-                ['rectangle', 'GmapRectangle'],
-                ['street-view-panorama', 'GmapStreetViewPanorama'],
-                ['extra-examples', 'Extra examples'],
-                ['cdn', 'Using a CDN'],
-              ]
+                ["map", "GmapMap"],
+                ["autocomplete", "GmapAutocomplete"],
+                ["circle", "GmapCircle"],
+                ["cluster", "GmapCluster"],
+                ["drawing-manager", "GmapDrawingManager"],
+                ["heatmap-layer", "GmapHeatmapLayer"],
+                ["info-window", "GmapInfoWindow"],
+                ["kml-layer", "GmapKmlLayer"],
+                ["marker", "GmapMarker"],
+                ["place-input", "GmapPlaceInput"],
+                ["polygon", "GmapPolygon"],
+                ["polyline", "GmapPolyline"],
+                ["rectangle", "GmapRectangle"],
+                ["street-view-panorama", "GmapStreetViewPanorama"],
+                ["extra-examples", "Extra examples"],
+                ["cdn", "Using a CDN"],
+              ],
             },
           ],
-        }
+        },
       ],
-      '/': [['', 'Getting started']]
-    }
-  }
+      "/": [["", "Getting started"]],
+    },
+  },
+  configureWebpack: (config, isServer) => {
+    configureWebpack: (config) => {
+      return { plugins: [new HtmlWebpackPlugin(), new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin)] };
+    };
+  },
 };
