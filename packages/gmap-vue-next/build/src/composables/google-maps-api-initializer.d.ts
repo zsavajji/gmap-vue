e69b0ed39740329fc919ee37e7d68f49584e9477
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2022-07-29 12:13:35 -0300

    chore(gmap-vue): remove unnecesary folder and files from the project

--- packages/gmap-vue-next/build/src/composables/google-maps-api-initializer.d.ts
+++
@@ -1,3 +0,0 @@
-import type { GoogleMapsAPIInitializerFn } from '@/types/gmap-vue.types';
-declare const _default: GoogleMapsAPIInitializerFn;
-export default _default;
--- packages/gmap-vue-next/build/src/composables/google-maps-promise.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="google.maps" />
-export declare function useGoogleMapsPromise(): Promise<{
-    map: google.maps.Map | undefined;
-}>;
-declare const _default: {
-    useMapElement: typeof useGoogleMapsPromise;
-};
-export default _default;
